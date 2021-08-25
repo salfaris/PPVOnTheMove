3285db6f3dddb724c5ffcfa523fd723850ebb992
Salman <salmanfaris98@gmail.com>
2021-08-25 16:23:46 +0100

    :white_check_mark: Added new test for n_centroid

--- test_clustering.py
+++ test_clustering.py
@@ -27,7 +27,16 @@ def test_is_numpy_array(df, n):
 ])
 def test_generate_exactly_n_centroids(df, n):
     assert len(n_centroid(df, n)) == n
-    
+
+@pytest.mark.parametrize("df, n", [
+    (even_len_df, len(even_len_df)),
+    (odd_len_df, len(odd_len_df)),
+])  
+def test_generate_centroids_less_than_equal_df(df, n):
+    # The extreme case is where we do exactly len(df) splits of df
+    # in which case len(n_centroid(df, n)) == len(df).
+    assert len(n_centroid(df, n)) <= len(df)
+
 @pytest.mark.parametrize("feature", [
     0.01, 0.1, 0.5, 1, 2021,
 ])
