4491abebd9deb69cfc1c030cf915414ad401f3be
Salman <salmanfaris98@gmail.com>
2021-08-25 15:10:17 +0100

    :sparkles: Added n_centroid function

--- clustering.py
+++ clustering.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pandas as pd
 
-PATH_TO_DATA = "data"
+SEED = 44
+PATH_TO_DATA = "data/"
+COORD_COLS = ['latitude', 'longitude']
+
+_data_src = PATH_TO_DATA + "ppv_skp-geo-pop-data.csv"
+spk_df = pd.read_csv(_data_src)
 
 def feature_to_radius(p: float):
     # This function is not determined yet and is subject to change,
@@ -9,8 +14,20 @@ def feature_to_radius(p: float):
     # has > 0 value.
     return max(np.log(p), 1)
 
-if __name__ == "__main__":
-    data_src = PATH_TO_DATA + "/ppv_skp-geo-pop-data.csv"
-
-    spk_df = pd.read_csv(data_src)
-    print(spk_df)
+def n_centroid(df: pd.DataFrame, n: int):
+    """Generates n centroids based on n splits of a uniformly
+    shuffled DataFrame."""
+    shuffled = df.sample(frac=1, random_state=SEED)
+    splits = np.array_split(shuffled, n)
+    
+    centroids = []
+    for split in splits:
+        # Latitude, longitude values of the split.
+        split_coords = split[COORD_COLS].values
+        # Weightless centroid (as we are using mean).
+        centroid = split_coords.mean(axis=0) 
+        centroids.append(centroid)
+    
+    return np.array(centroids)
+    
+    
--- test_clustering.py
+++ test_clustering.py
@@ -1,10 +1,35 @@
 import pytest
-from clustering import feature_to_radius
+import numpy as np
+import pandas as pd
+from clustering import feature_to_radius, n_centroid
 
-def test_greater_equal_one():
-    assert feature_to_radius(0.01) > 0
-    assert feature_to_radius(0.1) > 0
-    assert feature_to_radius(0.5) > 0
-    assert feature_to_radius(1) > 0
-    assert feature_to_radius(2021) > 0
+even_len_df = pd.DataFrame({
+    'latitude': np.arange(1, 11),
+    'longitude': np.random.randn(10),
+})
+odd_len_df = pd.DataFrame({
+    'latitude': np.arange(1, 10),
+    'longitude': np.random.randn(9),
+})
+
+@pytest.mark.parametrize("df, n", [
+    (even_len_df, 2),
+    (odd_len_df, 2),
+])
+def test_is_numpy_array(df, n):
+    assert isinstance(n_centroid(df, n), np.ndarray)
+
+@pytest.mark.parametrize("df, n", [
+    (even_len_df, 2),
+    (even_len_df, 3),
+    (odd_len_df, 4),
+    (odd_len_df, 5),
+])
+def test_generate_exactly_n_centroids(df, n):
+    assert len(n_centroid(df, n)) == n
     
+@pytest.mark.parametrize("feature", [
+    0.01, 0.1, 0.5, 1, 2021,
+])
+def test_greater_equal_one(feature):
+    assert feature_to_radius(feature) > 0
