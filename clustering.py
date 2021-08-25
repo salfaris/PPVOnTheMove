67269931a3c52a9bceef4eb057ca4b3c0bc0ddeb
Salman <salmanfaris98@gmail.com>
2021-08-25 15:56:16 +0100

    :adhesive_bandage: Fix bad variable name

--- clustering.py
+++ clustering.py
@@ -8,11 +8,11 @@ COORD_COLS = ['latitude', 'longitude']
 _data_src = PATH_TO_DATA + "ppv_skp-geo-pop-data.csv"
 spk_df = pd.read_csv(_data_src)
 
-def feature_to_radius(p: float):
+def feature_to_radius(feature: float):
     # This function is not determined yet and is subject to change,
     # and hence has limited testing. What is determined is that it 
     # has > 0 value.
-    return max(np.log(p), 1)
+    return max(np.log(feature), 1)
 
 def n_centroid(df: pd.DataFrame, n: int):
     """Generates n centroids based on n splits of a uniformly
