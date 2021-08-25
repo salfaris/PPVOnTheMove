54a9796a3bd17d544671f04c11272360334b99e9
Salman <salmanfaris98@gmail.com>
2021-08-25 12:56:42 +0100

    :sparkles: Added a function to filter ppv dataframe filtered on states.

--- clustering.py
+++ clustering.py
@@ -1,8 +1,28 @@
 import numpy as np
 import pandas as pd
 
+PATH_TO_DATA = "data"
+
+def get_dataframe_for_states(df: pd.DataFrame, states: list):
+    """Returns a filtered df if df.state contains `states`.
+    Assumes that df contains `state` as a column.
+    """
+    return df[df.state.isin(states)]
+
 def feature_to_radius(p: float):
     # This function is not determined yet and is subject to change,
     # and hence has limited testing. What is determined is that it 
-    # has > 0 radius.
+    # has > 0 value.
     return max(np.log(p), 1)
+
+if __name__ == "__main__":
+    data_src = PATH_TO_DATA + "/ppv_state-district-geo-dataAug2021.csv"
+
+    ppv_df = pd.read_csv(data_src)
+    spk_states = [
+        "Selangor",
+        "W.P. Kuala Lumpur",
+        "W.P. Putrajaya",
+    ]
+    spk_df = get_dataframe_for_states(ppv_df, spk_states)
+    print(spk_df)
--- test_clustering.py
+++ test_clustering.py
@@ -7,3 +7,4 @@ def test_greater_equal_one():
     assert feature_to_radius(0.5) > 0
     assert feature_to_radius(1) > 0
     assert feature_to_radius(2021) > 0
+    
